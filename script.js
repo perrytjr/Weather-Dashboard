e7ed86e24400a9b4aa991c939568708b3079b9b3
Perry Theobald <perrytjr@gmail.com>
2020-08-10 09:40:58 -0700

    one city button is now created when clicked on search button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 //my api key from the openweathermap.org
 let APIKey = "4f8564279b0a48b567bd6ba7e52a8a22";
-let cityList = document.getElementById("citylist");
+//let cityList = document.getElementById("list-of-cities");
 //empty array for cities
 let cities = [];
 let yourcity= "";
@@ -11,8 +11,8 @@ saveCities();
 
 function getCities () {
     let cityStorage = JSON.parse(localStorage.getItem("cities"));
-    if (cityStorage !== null){
-        cities = cityStorage
+    if (cityStorage === null || cityStorage === undefined){
+        return;
     }
     makecitybuttons();
 }
@@ -25,17 +25,17 @@ function saveCities () {
  
 function makecitybuttons() {
     $("#list-of-cities").innerHTML ="";
-    if(cities == null){
+    if(cities === null){
         return;
     }
+    $("#list-of-cities").empty();
     let specificCities = [...new Set (cities)];
-    for( let i = 0; i < specificCities.length; i++){
+    for (var i = 0; i < specificCities.length; i++){
         let newcityName = specificCities[i];
 
         let buttonElement = document.createElement("button");
         buttonElement.textContent = newcityName;
-        buttonElement.setAttribute("class", "listbutton");
-        
+        buttonElement.setAttribute("class", "listbutton btn btn-outline-secondary btn-block");
         $("#list-of-cities").append(buttonElement);
         cityButton();
     }
@@ -44,8 +44,7 @@ function makecitybuttons() {
 function cityButton () {
     $("listbutton").on("click", function (event){
         event.preventDefault();
-        
-        cityList = $("#city-name").text().trim();
+        cityList = $("#city-name").text();
     })
 }
 
@@ -53,7 +52,7 @@ function searchCities(){
     $("#search-button").on("click", function(event){
         console.log("#search-button");
         event.preventDefault();
-        yourcity = $("#city-name").val().trim();
+        yourcity = $("#city-name").val();
         cities.push(yourcity);
 
         //we only want 5 cities saved so .shift will remove the first saved city allow for a new one to be displayed 
@@ -61,7 +60,7 @@ function searchCities(){
             cities.shift()
         }
         if (yourcity == "") {
-            return;
+        return;
         }
         saveCities();
         makecitybuttons();
